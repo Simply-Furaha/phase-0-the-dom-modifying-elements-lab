791d53cdd6f451ed21a0aa46ce42384fdd8969c3
Maxwell Furaha <maxwellfuraha91@gmail.com>
2024-06-22 13:48:38 +0300

    Working on Dom Manipulation

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+
+const newHeader = document.createElement('h1');
+
+newHeader.textContent = 'Max is the champion';
+
+newHeader.id = 'victory';
+
+document.body.appendChild(newHeader);
